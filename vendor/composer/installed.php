f4cbff93f85fe0402a791c6d6fc80913a9289a3a
tiras.muturi <tiras.muturi@radioafricagroup.co.ke>
2022-07-22 15:05:42 +0300

    First Commit : consolidated package that handles most payment integrations

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'tirasmuturi/consolidatedpayments',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'tirasmuturi/consolidatedpayments' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+    ),
+);
